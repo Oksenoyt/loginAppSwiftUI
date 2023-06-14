491de7b479656deb9fa6d06d9a62b178c052737c
Oksenoyt <a.oksenoyt@gmail.com>
2023-06-14 15:07:31 -0700

    Initial Commit

---
+++ loginAppSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  loginAppSwiftUI
+//
+//  Created by Elenka on 14.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loginAppSwiftUI/loginAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginAppSwiftUIApp.swift
+//  loginAppSwiftUI
+//
+//  Created by Elenka on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct loginAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
